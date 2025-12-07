8fd7adc9652119bda3ee2b805b216102beaeecab
Turgay Apaydın <bodiess@gmail.com>
2025-12-07 12:27:21 +0300

    Update

--- app/page.js
+++ app/page.js
@@ -763,7 +763,6 @@ export default function HomePage() {
 
                 {/* Results grid */}
                 {analysisResult && (
-                  {analysisResult && (
   <PerceptionSurvey analysis={analysisResult} originalText={decisionText} />
 )}
 
